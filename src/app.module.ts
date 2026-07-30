dffe847e2a3b236048974785b8b0b35b4ed6daee
Diego Alejandro Diaz <90485793+AlejandroDeveloper2@users.noreply.github.com>
2026-07-30 12:24:18 -0500

    chore:features created and initialized

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
+
 import { AppService } from './app.service';
 
+import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './auth/auth.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { ProductsModule } from './products/products.module';
+import { InventoryModule } from './inventory/inventory.module';
+import { UsersModule } from './users/users.module';
+import { CategoriesModule } from './categories/categories.module';
+import { OrdersModule } from './orders/orders.module';
+import { ClientsModule } from './clients/clients.module';
+import { BranchesModule } from './branches/branches.module';
+
 @Module({
-  imports: [],
+  imports: [
+    SharedModule,
+    AuthModule,
+    DashboardModule,
+    ProductsModule,
+    InventoryModule,
+    UsersModule,
+    CategoriesModule,
+    OrdersModule,
+    ClientsModule,
+    BranchesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
