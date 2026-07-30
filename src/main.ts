86b9aa830a333935dc483d34a66e3e14708d59bd
Diego Alejandro Diaz <90485793+AlejandroDeveloper2@users.noreply.github.com>
2026-07-30 12:53:37 -0500

    chore:cors initial configuration

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
