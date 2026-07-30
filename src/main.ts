ee6fa8c54f54d3a500f932dc2549bdaf88c120af
Diego Alejandro Diaz <90485793+AlejandroDeveloper2@users.noreply.github.com>
2026-07-30 15:03:58 -0500

    chore: configure global prefix, versioning, and cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,37 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  /** Set global prefix */
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  /** Cookie parser */
+  app.use(cookieParser());
+
   /** Cors settings */
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Accept, Authorization',
+    credentials: true,
   });
-  /** Cookie parser */
-  app.use(cookieParser());
+
   /** Global validation pipe */
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
+
   /** Start the server */
   await app.listen(process.env.PORT ?? 3000);
 }
